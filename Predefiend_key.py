7ec414be3cb7d1ab78396906db6da4d09d7d4d73
jittupal <jksuperking3@gmail.com>
2024-12-30 06:51:12 +0530

    first commit

---
+++ Predefiend_key.py
@@ -0,0 +1,35 @@
+print("hello world")
+a = 3
+print(a)
+b = 3.5
+print(type(b))
+ # type tell what is the datatype we used
+c = a * "hii"  #this will print hii 3 times because a has value 3
+print(c)
+print(type(c))
+
+com = 4+3j
+print(type(com))
+print(com.real) #real number of complex value
+print(com.imag) #imaginary number of complex value
+
+''' 
+Rule of defining a variable
+1. we can start with letter or underscore but we can not use number or anything else
+
+example:
+
+3 = a # this will give you error
+1name = "ajay" #this will also give error
+data-analytics = "anything" #this will also give error
+
+correct ways:
+name = "ajay"
+value = 5
+data_analytics = "anything"
+
+
+'''
+
+a = "jittu"
+print(a)
