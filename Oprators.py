1128428e5af923c6dfc03088cebdffae9678e3c0
jittupal <jksuperking3@gmail.com>
2025-01-04 06:02:43 +0530

    first

---
+++ Oprators.py
@@ -0,0 +1,21 @@
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) # this will give remainder after dividing the number
+print(a // b) # this // is a floor oprators, it round of the value to it's nearest integer
+print(a ** b) # ** is used for know the power eg. a to power b
+
+# *****COMMPARISION OPERATORS****
+print(a > b) #is a greater than b
+print(a < b) #is a less than b
+print(a == b) #is a equal to b
+print(a >= b) #is a greater than or equal to b
+print(a <= b) #is a less than or equal to b
+print(a != b) #is a not equal to be
+
+
+#******Logical Operators******
