3fe43f1eebe6c24405931936fdc4e25547df0cd0
jittupal <jksuperking3@gmail.com>
2025-03-17 20:51:55 +0530

    commit

--- Python_Basics_Assignment.py
+++ Python_Basics_Assignment.py
@@ -29,3 +29,34 @@ for i in range(5):
         continue  
     print(i)
 
+my_list = [1, 2, 3]
+print("Before modification:", my_list)
+
+my_list[0] = 10  # Modifies the first element
+print("After modification:", my_list)
+
+my_dict = {'name': 'Alice', 'age': 25}
+print("Before modification:", my_dict)
+
+my_dict['age'] = 26  # Changing value of an existing key
+my_dict['city'] = 'New York'  # Adding a new key-value pair
+print("After modification:", my_dict)
+
+my_set = {1, 2, 3}
+print("Before modification:", my_set)
+
+my_set.add(4)  # Adding a new element
+my_set.remove(2)  # Removing an element
+print("After modification:", my_set)
+
+
+my_string = "Hello"
+print("Original string:", my_string)
+
+# Trying to modify a string
+# my_string[0] = 'h'  #This will cause an error
+
+my_tuple = (1, 2, 3)
+print("Original tuple:", my_tuple)
+
+# my_tuple[0] = 10  # This will cause an error
